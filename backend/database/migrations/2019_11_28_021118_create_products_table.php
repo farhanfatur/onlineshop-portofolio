c28da50b61dacbc5d49aea13ae7cc9d5ebea4b81
Farhan Fatur <farhan@kanosolution.com>
2022-04-06 14:57:17 +0700

    update backend laravel

---
+++ backend/database/migrations/2019_11_28_021118_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('name_slug');
+            $table->string('code');
+            $table->integer('stock');
+            $table->unsignedBigInteger('category_id');
+            $table->text('description');
+            $table->string('image');
+            $table->unsignedBigInteger('seller_id');
+            $table->integer('price');
+            $table->enum('is_active', ['1', '0']);
+            $table->enum('is_sold', ['1', '0']);
+            $table->enum('is_delete', ['1', '0']);
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('seller_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
